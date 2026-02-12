6ef044c1a5fe08f979c3b41e7ac5bc66a7ee622a
mateo <thesaintszn@gmail.com>
2026-02-12 19:14:38 +0100

    commit 1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('@rainbow-me') || id.includes('wagmi') || id.includes('viem')) {
+               return 'vendor-web3';
+            }
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
   server: {
     proxy: {
       '/api': {
