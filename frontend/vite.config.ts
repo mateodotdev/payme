51e254b3da8dad790bb2511b4bab3a42889e12dc
mateo <thesaintszn@gmail.com>
2026-02-15 08:42:46 +0100

    v1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,11 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes('@rainbow-me') || id.includes('wagmi') || id.includes('viem')) {
+            if (id.includes('@reown') || id.includes('wagmi') || id.includes('viem')) {
                return 'vendor-web3';
             }
             return 'vendor';
